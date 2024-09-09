7fc1408346d78a72bf0712c9a2d29bcb2dfdfdef
Timothy Adebisi <adebisis539@gmail.com>
2024-09-09 22:12:41 +0100

    database config settings implementation

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
-from dotenv import load_dotenv, dotenv_values
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
+
+    class Config:
+        env_file = ".env" 
+
+settings = Settings()
