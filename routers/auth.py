517e7504e5c2857119b41107a30ff05548de24b8
Timothy Adebisi <adebisis539@gmail.com>
2024-09-11 23:36:13 +0100

    Add passlib abd cryptContext

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from schemas import UserCreate
+from models import Users
+from passlib.context import CryptContext
+
 
 router = APIRouter(
     prefix='/auth',
     tags=['Autentication']
 )
 
-@router.post('/create_user')
-async def create_user():
-    return {'request': 'User Created'}
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+@router.post('/create_user', status_code=status.HTTP_200_OK)
+async def signup(request: UserCreate):
+    user = UserCreate(
+        first_name=request.first_name,
+        last_name= request.last_name,
+        username=request.username,
+        password=bcrypt_context.hash(request.password)
+    )
+    return user
