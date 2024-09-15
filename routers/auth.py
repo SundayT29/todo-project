a2f8655a46eb92aa7aa416ac36c8de12dd8d9ba7
Timothy Adebisi <adebisis539@gmail.com>
2024-09-15 21:01:04 +0100

    updates

--- routers/auth.py
+++ routers/auth.py
@@ -4,6 +4,7 @@ from schemas import UserRequest
 from models import Users
 from passlib.context import CryptContext
 from services import get_user
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter(
@@ -13,7 +14,7 @@ router = APIRouter(
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-@router.post('/create_user', status_code=status.HTTP_200_OK)
+@router.post('/create_user', status_code=status.HTTP_201_CREATED)
 async def signup(request: UserRequest, db=Depends(get_db)):
     user = Users(
         first_name=request.first_name,
@@ -25,9 +26,13 @@ async def signup(request: UserRequest, db=Depends(get_db)):
     user_exists = get_user(request=user, db=db)
 
     if user_exists:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User Already Exist!")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username Already Exist!")
 
     db.add(user)
     db.commit()
     db.refresh(user)
     return {'id': user.uuid, 'username': user.username, 'role': user.role}
+
+@router.post('/token', status_code=status.HTTP_200_OK)
+async def get_token():
+    pass
